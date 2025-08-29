499d53c7b901ee37be7dc73bd525859a2453dbd2
Abinash Regmi <abinashregmi01234@gmail.com>
2025-08-29 15:29:41 +0000

    ok

--- app/app.py
+++ app/app.py
@@ -1,20 +1,16 @@
-import streamlit as st
-import pandas as pd
 import base64
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy
+import pandas as pd
+import streamlit as st
 
-st.title('NBA Player Stats Explorer')
-
+st.title("NBA Player Stats Explorer")
 st.markdown("""
-This app performs simple webscraping of NBA player stats data!
+This app performs simple web scraping of NBA player stats data!
 * **Python libraries:** base64, pandas, streamlit
-* **Data source:** [Basketball-reference.com](https://www.basketball-reference.com/).
+* **Data source:** [Basketball-reference.com](https://www.basketball-reference.com/)
 """)
 
-st.sidebar.header('User Input Features')
-selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950,2020))))
+st.sidebar.header("User Input Features")
+selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950, 2025))))
 
 @st.cache_data
 def load_data(year):
@@ -39,41 +35,15 @@ def load_data(year):
     return playerstats, team_col
 
 playerstats, team_col = load_data(selected_year)
-if team_col:
+
+if team_col and team_col in playerstats.columns:
     sorted_unique_team = sorted(playerstats[team_col].unique())
     selected_team = st.sidebar.multiselect('Team', sorted_unique_team, sorted_unique_team)
+else:
+    sorted_unique_team = []
+    selected_team = []
 
-# Sidebar - Position selection
-unique_pos = ['C','PF','SF','PG','SG']
-selected_pos = st.sidebar.multiselect('Position', unique_pos, unique_pos)
+if selected_team:
+    playerstats = playerstats[playerstats[team_col].isin(selected_team)]
 
-# Filtering data
-df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_pos))]
-
-st.header('Display Player Stats of Selected Team(s)')
-st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
-st.dataframe(df_selected_team)
-
-# Download NBA player stats data
-# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
-def filedownload(df):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-    href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
-    return href
-
-st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
-
-# Heatmap
-if st.button('Intercorrelation Heatmap'):
-    st.header('Intercorrelation Matrix Heatmap')
-    df_selected_team.to_csv('output.csv',index=False)
-    df = pd.read_csv('output.csv')
-
-    corr = df.corr()
-    mask = np.zeros_like(corr)
-    mask[np.triu_indices_from(mask)] = True
-    with sns.axes_style("white"):
-        f, ax = plt.subplots(figsize=(7, 5))
-        ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
-    st.pyplot()
+st.dataframe(playerstats)
